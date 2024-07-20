6c27965b9cefe1f78a4431c329e6c50e5cc4aaac
guangyusong <15316444+guangyusong@users.noreply.github.com>
2024-07-20 17:17:01 -0400

    Test out rwkv 4,5,6

---
+++ zoology/experiments/examples/rwkv6.py
@@ -0,0 +1,25 @@
+from zoology.config import TrainConfig, ModelConfig, DataConfig, ModuleConfig
+from zoology.data.associative_recall import MQARConfig
+
+factory_kwargs = {
+        "num_kv_pairs": 4,
+    }
+
+config = TrainConfig(
+    data=DataConfig(
+        # cache_dir="/path/to/cache/dir"  TODO: add this
+        train_configs=[MQARConfig(num_examples=10_000, vocab_size=256, input_seq_len=64, **factory_kwargs)],
+        test_configs=[MQARConfig(num_examples=1_000, vocab_size=256, input_seq_len=64, **factory_kwargs)],
+    ),
+    model=ModelConfig(
+        vocab_size=256,
+        max_position_embeddings=64,
+        sequence_mixer=ModuleConfig(
+            name="zoology.mixers.rwkv6.RWKV_Tmix_x060",
+            kwargs={"l_max": 8}
+        )
+    ),
+    
+)
+
+configs = [config]
